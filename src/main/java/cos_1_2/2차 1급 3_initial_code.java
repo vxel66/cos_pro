c142d21d61133d0080157b0e547f36126de5c5c1
vxel66 <vxel7408@gmail.com>
2022-03-03 17:53:39 +0900

    123

--- src/main/java/cos_1_2/2차 1급 3_initial_code.java
+++ src/main/java/cos_1_2/2차 1급 3_initial_code.java
@@ -48,19 +48,16 @@ class Solution3 {
         return nextNum - num;
     }
 
-    // 아래는 테스트케이스 출력을 해보기 위한 main 메소드입니다.
     public static void main(String[] args) {
         Solution3 sol = new Solution3();
         int num1 = 1;
         int ret1 = sol.solution(num1);
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
 
         int num2 = 235386;
         int ret2 = sol.solution(num2);
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
     }
 }
